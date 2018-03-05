fade794dfdf0c6fc2bdf1243bf375e0bf34234f3
EirikOsnes <eirik.osnes@gmail.com>
2018-03-05 11:51:37 +0100

    Initial commit

---
+++ desktop/src/com/panic/tdt4240/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.panic.tdt4240.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.panic.tdt4240.PanicGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PanicGame(), config);
+	}
+}

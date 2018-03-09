fb9181ebeaa886aad8f2268724cee24f90b9d47b
Hermann Villanger <hermannvillanger@gmail.com>
2018-03-09 15:30:25 +0100

    Added class with GlobalConstants Added constant for cards drawn and played each round

---
+++ core/src/com/panic/tdt4240/util/GlobalConstants.java
@@ -0,0 +1,16 @@
+package com.panic.tdt4240.util;
+
+/**
+ * Created by Hermann on 09.03.2018.
+ */
+
+public class GlobalConstants {
+
+    //Base number of cards drawn each round
+    public static final int BASE_DRAW_CARDS = 5;
+    //Base number of cards played each round
+    public static final int BASE_PLAY_CARDS = 3;
+
+
+
+}

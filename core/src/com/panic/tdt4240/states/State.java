a98a45c3d649c64e541e37af44f1b858ce29a87c
Magnleik <magnleik@stud.ntnu.no>
2018-03-12 14:37:28 +0100

    moved camera to view from state

--- core/src/com/panic/tdt4240/states/State.java
+++ core/src/com/panic/tdt4240/states/State.java
@@ -1,6 +1,5 @@
 package com.panic.tdt4240.states;
 
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
@@ -9,13 +8,11 @@ import com.badlogic.gdx.math.Vector3;
  */
 
 public abstract class State {
-    protected OrthographicCamera cam;
     protected Vector3 mouse;
     protected GameStateManager gsm;
 
     protected State(GameStateManager gsm){
         this.gsm = gsm;
-        cam = new OrthographicCamera();
         mouse = new Vector3();
     }
 
--- core/src/com/panic/tdt4240/view/ViewClasses/AbstractView.java
+++ core/src/com/panic/tdt4240/view/ViewClasses/AbstractView.java
@@ -1,6 +1,7 @@
 package com.panic.tdt4240.view.ViewClasses;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.panic.tdt4240.states.State;
 
 /**
@@ -16,10 +17,12 @@ import com.panic.tdt4240.states.State;
 
 public abstract class AbstractView {
     protected State state;
+    protected OrthographicCamera cam;
     public static final int SCREEN_WIDTH = Gdx.graphics.getWidth();
     public static final int SCREEN_HEIGHT = Gdx.graphics.getHeight();
 
     public AbstractView(State state){
+        cam = new OrthographicCamera();
         this.state = state;
         //Insert whatever should be used by multiple subclasses
     }

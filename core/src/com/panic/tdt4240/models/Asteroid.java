44d9f9d0cc8350ab55f992450630542510b49423
choffa <cgjovag@gmail.com>
2018-03-12 13:42:16 +0100

    Added handleEvent

--- core/src/com/panic/tdt4240/models/Asteroid.java
+++ core/src/com/panic/tdt4240/models/Asteroid.java
@@ -1,8 +1,20 @@
 package com.panic.tdt4240.models;
 
+import com.panic.tdt4240.events.Event;
+import com.panic.tdt4240.events.EventListener;
+
 /**
  * Created by Eirik on 05-Mar-18.
  */
 
-public class Asteroid {
+public class Asteroid implements EventListener{
+
+
+
+    @Override
+    public void handleEvent(Event e) {
+        if (e.getT() == Event.Type.ATTACK) {
+            //TODO: Handle attack event
+        }
+    }
 }

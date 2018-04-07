011553b2bec447feaa477bde7d3c224acedbb2ce
EirikOsnes <eirik.osnes@gmail.com>
2018-04-07 13:42:51 +0200

    Set up GameModelHolder

---
+++ core/src/com/panic/tdt4240/states/LoadGameState.java
@@ -0,0 +1,33 @@
+package com.panic.tdt4240.states;
+
+/**
+ * Created by Eirik on 07-Apr-18.
+ */
+
+public class LoadGameState extends State {
+
+
+    protected LoadGameState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput(Object o) {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}

91fac945a5d412e4ad805f8de0d0ca5f19b0da32
Magnleik <magnleik@stud.ntnu.no>
2018-04-18 12:25:38 +0200

    Added missile sequence to attack vehicle method

--- core/src/com/panic/tdt4240/states/RunEffectsState.java
+++ core/src/com/panic/tdt4240/states/RunEffectsState.java
@@ -1,7 +1,6 @@
 package com.panic.tdt4240.states;
 
 import com.badlogic.gdx.Gdx;
-import com.panic.tdt4240.connection.Connection;
 import com.panic.tdt4240.connection.ICallbackAdapter;
 import com.panic.tdt4240.events.Event;
 import com.panic.tdt4240.events.EventBus;
@@ -11,12 +10,9 @@ import com.panic.tdt4240.models.Card;
 import com.panic.tdt4240.models.GameInstance;
 import com.panic.tdt4240.models.ModelHolder;
 import com.panic.tdt4240.view.ViewClasses.AbstractView;
-import com.panic.tdt4240.view.ViewClasses.PlayCardView;
 import com.panic.tdt4240.view.ViewClasses.RunEffectsView;
 
 import java.util.ArrayList;
-
-import javax.swing.text.View;
 
 /**
  * State for running through the cards gotten from the server.
@@ -75,7 +71,7 @@ public class RunEffectsState extends State implements EventListener {
                 runEffectsView.attackAsteroid(e.getTargetID());
             }
             else if (e.getTargetID().matches("V-\\d\\d\\d")) {
-                runEffectsView.attackVehicle(e.getTargetID());
+                runEffectsView.attackVehicle(e.getTargetID(),e.getInstigatorID());
             }
         }
         else if (e.getT() == Event.Type.MOVE) {

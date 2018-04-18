2719539a29a43f5816a52c7a7965ec86a1ac03e4
Quint-V <vicweiwan@gmail.com>
2018-04-18 13:05:10 +0200

    Fixing scroller

--- core/src/com/panic/tdt4240/view/ViewClasses/GameListView.java
+++ core/src/com/panic/tdt4240/view/ViewClasses/GameListView.java
@@ -32,7 +32,7 @@ public class GameListView extends AbstractView {
     private Skin skin;
     private BitmapFont font;
     private TextButton.TextButtonStyle btnStyle;
-    private Table table, exitTable;
+    private Table lobbyBtnTable, exitTable;
     private Texture bg;
     private TextButton exitToMainMenuBtn;
 
@@ -65,10 +65,10 @@ public class GameListView extends AbstractView {
     }
 
     public void updateView(){
-        table = new Table();
-        table.setFillParent(true);
-        table.background(new TextureRegionDrawable(new TextureRegion(bg)));
-        table.center();
+        lobbyBtnTable = new Table();
+        lobbyBtnTable.setFillParent(true);
+        lobbyBtnTable.background(new TextureRegionDrawable(new TextureRegion(bg)));
+        lobbyBtnTable.center();
 
         for (int i = 0; i < listState.getLobbyListData().size(); i++){
             final String data[] = listState.getLobbyListData().get(i);
@@ -79,23 +79,28 @@ public class GameListView extends AbstractView {
                     state.handleInput(data[1]); // lobbyID
                 }
             });
-            table.add(button).width(300).pad(5); table.row();
+            lobbyBtnTable.add(button).width(300).pad(5); lobbyBtnTable.row();
         }
-        if (listState.getLobbyListData().size() != 0) {
-            createExitToMainMenuBtn();
-            exitTable = new Table();
-            exitTable.setFillParent(true);
-            exitTable.bottom();
-            exitTable.add(exitToMainMenuBtn).padTop(30).padBottom(30).bottom();
-            exitTable.pack();
-            table.pack();
-            stage.addActor(table);
-            stage.addActor(exitTable);
-        }
-//        scroller = new ScrollPane(table);
-//        scroller.setScrollingDisabled(true, false);
-//        table.add(scroller).fill().expand();
-//        stage.addActor(scroller);
+        // scrolls child widgets.
+        scroller = new ScrollPane(lobbyBtnTable);
+        ScrollPane.ScrollPaneStyle scrollStyle = new ScrollPane.ScrollPaneStyle();
+        scrollStyle.background = skin.getDrawable("default-rect");
+        scrollStyle.vScroll = skin.getDrawable("default-scroll");
+        scrollStyle.vScrollKnob = skin.getDrawable("default-round-large");
+        scroller.setScrollingDisabled(true, false);
+        scroller.setStyle(scrollStyle);
+        stage.addActor(scroller);
+
+        createExitToMainMenuBtn();
+        exitTable = new Table();
+        exitTable.setFillParent(true);
+        exitTable.bottom();
+        exitTable.add(exitToMainMenuBtn).padTop(30).padBottom(30).bottom();
+        exitTable.pack();
+        lobbyBtnTable.pack();
+
+        stage.addActor(scroller);
+        stage.addActor(exitTable);
     }
 
     private void createExitToMainMenuBtn(){

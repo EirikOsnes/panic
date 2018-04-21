056bfa516c54f522f8d2b757a3599efc579b1ff1
Quint-V <vicweiwan@gmail.com>
2018-04-21 20:24:08 +0200

    Added comments

--- core/src/com/panic/tdt4240/view/ViewClasses/LoadGameView.java
+++ core/src/com/panic/tdt4240/view/ViewClasses/LoadGameView.java
@@ -5,6 +5,13 @@ import com.panic.tdt4240.view.ViewClasses.AbstractView;
 
 /**
  * Created by Hermann on 17.04.2018.
+ * 
+ * This view was supposed to show a loading indicator, e.g. a rotating
+ * refresh symbol similar to server browsers' refresh button.
+ *
+ * As it is not important for the application, it was mostly ignored
+ * and left as is, also due to time constraints.
+ *
  */
 
 public class LoadGameView extends AbstractView {

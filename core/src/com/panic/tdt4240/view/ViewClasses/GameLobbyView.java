1c9633cf508efe869eca4ac24969f4558c23290a
EirikOsnes <eirik.osnes@gmail.com>
2018-06-14 22:06:44 +0200

    Fixed bug with drop down menu for seleceting vehicle not working

--- core/src/com/panic/tdt4240/view/ViewClasses/GameLobbyView.java
+++ core/src/com/panic/tdt4240/view/ViewClasses/GameLobbyView.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -15,8 +16,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.panic.tdt4240.PanicGame;
 import com.panic.tdt4240.connection.Connection;
 import com.panic.tdt4240.models.ModelHolder;
@@ -74,7 +75,7 @@ public class GameLobbyView extends AbstractView {
         lobbyName="";
         scl = 2;
         this.lobbyState=lobbyState;
-        /** INIT SETUP */
+        /* INIT SETUP */
 
         bg = new Texture("misc/background.png");
         cam.setToOrtho(false, PanicGame.WIDTH,PanicGame.HEIGHT);
@@ -89,7 +90,7 @@ public class GameLobbyView extends AbstractView {
 
         generateBottomItems();
 
-        /** INIT SETUP END */
+        /* INIT SETUP END */
     }
 
     /** RUNS AFTER LOBBY DATA HAS BEEN UPDATED
@@ -104,7 +105,6 @@ public class GameLobbyView extends AbstractView {
         // Draw these when data is loaded or updated
         if (lobbyState.isDataLoaded()) {
             preparePlayerListDisplay(); // generates playerTxtFields
-            prepareSelectVehicleDisplay();
             lobbyName=lobbyState.getLobby().getLobbyname();
         }
         else {
@@ -118,7 +118,7 @@ public class GameLobbyView extends AbstractView {
         // skin.getDrawable("textfield") generates the usual label background.
         txtStyle = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
         txtStyle.background = skin.getDrawable("textfield");
-        System.out.println(GlobalConstants.GET_TEXT_SCALE());
+        System.out.println("Text Scale: " + GlobalConstants.GET_TEXT_SCALE());
 
         rdyStyle = new Label.LabelStyle(font, Color.BLACK);
         rdyStyle.background = skin.getDrawable("button-ready");
@@ -131,6 +131,7 @@ public class GameLobbyView extends AbstractView {
         backgroundActor = new Image(new TextureRegion(bg));
         backgroundActor.setZIndex(0);
         backgroundActor.setSize(stage.getWidth(), stage.getHeight());
+        backgroundActor.addListener(new BackGroundClickListener().setField("Background"));
         stage.addActor(backgroundActor);
 
         boxStyle = new SelectBox.SelectBoxStyle(skin.get(SelectBox.SelectBoxStyle.class));
@@ -140,6 +141,22 @@ public class GameLobbyView extends AbstractView {
         txtStyle.font.getData().scale(GlobalConstants.GET_TEXT_SCALE() * 1.5f);
         rdyStyle.font.getData().scale(GlobalConstants.GET_TEXT_SCALE() * scl);
         btnStyle.font.getData().scale(GlobalConstants.GET_TEXT_SCALE() * scl);
+    }
+
+    private class BackGroundClickListener extends ClickListener{
+
+        private String field;
+
+        public BackGroundClickListener setField(String field) {
+            this.field = field;
+            return this;
+        }
+
+        @Override
+        public void clicked(InputEvent event, float x, float y) {
+            System.out.println(field + " clicked");
+            super.clicked(event, x, y);
+        }
     }
 
     private void preparePlayerListDisplay(){
@@ -182,34 +199,23 @@ public class GameLobbyView extends AbstractView {
 
     }
 
-    private void prepareSelectVehicleDisplay(){
-//        String[] carTypes = {"HURR", "DURR", "HERP", "DERP"}; // USED FOR TESTING
+    private SelectBox<String> createVehicleSelector(){
+
         String[] carTypes = getVehicleNames();
-        carSelectBox = new SelectBox<>(skin);
-
-        // scaleby: adds to the current scale. setScale --> defines the number.
-        // sizeby: adds to the size. setsize --> define the size (width, height).
-        carSelectBox.setStyle(boxStyle);
-        carSelectBox.setSize(SCREEN_WIDTH*4/5, SCREEN_HEIGHT/16);
-        carSelectBox.setScale(SCREEN_WIDTH*4/5, GlobalConstants.GET_TEXT_SCALE());
-
-
-        carSelectBox.getScrollPane().scaleBy(GlobalConstants.GET_TEXT_SCALE() * 2);
-
-//        carSelectBox.getScrollPane().setWidth(SCREEN_WIDTH*4/5);
-        carSelectBox.setItems(carTypes);
-        carSelectBox.setSelectedIndex(0); // default value
-        carSelectBox.setWidth(SCREEN_WIDTH/2);
-        carSelectBox.addListener(new ChangeListener() {
+        final SelectBox<String> selector = new SelectBox<String>(skin);
+        selector.setStyle(boxStyle);
+        selector.setItems(carTypes);
+        selector.getScrollPane().scaleBy(GlobalConstants.GET_TEXT_SCALE());
+        selector.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
+                System.out.println("Changing vehicle type to " + selector.getSelected());
             }
         });
-        carSelectBox.setPosition(SCREEN_WIDTH/2 - carSelectBox.getWidth()/4, SCREEN_HEIGHT/10 * 2);
-        carSelectBox.pack();
+        selector.setSelectedIndex(0);
+        selector.pack();
 
-        stage.addActor(carSelectBox);
-
+        return selector;
     }
 
     private void generateBottomItems(){
@@ -267,10 +273,16 @@ public class GameLobbyView extends AbstractView {
         exitBtn.pack();
 
         Table bottomTable = new Table();
+        //bottomTable.setDebug(true,true);
         bottomTable.center();
-        bottomTable.add(readyBtn).width(SCREEN_WIDTH/3).height(SCREEN_HEIGHT*0.08f).pad(20);
-        bottomTable.add(exitBtn).width(SCREEN_WIDTH/3).height(SCREEN_HEIGHT*0.08f).pad(20);
+        carSelectBox = createVehicleSelector();
+        bottomTable.add(carSelectBox).pad(20).width(SCREEN_WIDTH/2);
         bottomTable.row();
+        Table bottomButtonTable = new Table();
+        bottomButtonTable.add(readyBtn).width(SCREEN_WIDTH/3).height(SCREEN_HEIGHT*0.08f).pad(20);
+        bottomButtonTable.add(exitBtn).width(SCREEN_WIDTH/3).height(SCREEN_HEIGHT*0.08f).pad(20);
+        bottomButtonTable.padBottom(20);
+        bottomTable.add(bottomButtonTable);
         bottomTable.setPosition(SCREEN_WIDTH/2, SCREEN_HEIGHT/10);
 
         stage.addActor(bottomTable);
